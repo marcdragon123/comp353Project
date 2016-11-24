38d59be3f5cee995a344ae38c6aa82e9c4e798ce
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2016-11-24 15:44:08 -0500

    ..

--- register.php
+++ register.php
@@ -1,29 +1,41 @@
-<?php include 'header.php';
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function Register(string $Email, string $dob, string $FirstName, string $Lastname, string $Address, string $City, string $Password)
+{
+    $query1 = NULL;
+    $query2 = NULL;
+    if (IsValide($Email) && IsValide($Password))
+    {
+        $query1 = "INSERT INTO `users` (`email`,`password`,`isAdmin`,`status`,`balance`) VALUES (`".$Email."`,`".$Password."`,`false`,`null,`0.0`)";
+    }
+    if(IsValide($dob) && IsValide($FirstName) && IsValide($Lastname) && IsValide($Address) && IsValide($City) && IsValide($query))
+    {
+        $query2 = "INSERT INTO `personalinfo`(`user_email`,`firstname`,`lastname`,`address`,`city`,`dob`) VALUES (`".$Email."`,`".$FirstName."`,`".$Lastname."`,`".$Address."`,`".$City."`,`".$dob."`)";
+    }
+    
+    if (IsValide($query2) && IsValide($query1))
+    {
+        $db->query($query1);
+        $db->query($query2);
+    }
+}
+
+function IsValide(string $input)
+{
+    if (!empty($input) && !strpos($input,'DROP'))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 ?>
-	<h1>Register</h1>
-<?php 
-	if(isset($_POST['username'])){
-		$result=userExist($_POST['username']);
-		if($result){
-			echo '<p>'.$result.'</p>';
-		}
-		else{
-			$hashpassword = password_hash($_POST['password'],PASSWORD_DEFAULT);
-			query("INSERT INTO users (username,password) values ('".$_POST['username']."','".$hashpassword."')");
-			query("INSERT INTO personalinfo(username,firstname,lastname,address,city,dob) values ('".$_POST['username']."','".$_POST['fname']."','".$_POST['lname']."','".$_POST['address']."','".$_POST['city']."','".$_POST['dbo']."')");
-			$_SESSION['user']=$_POST['username'];
-			header("Location:home.php");
-		}
-	}
-;?>
-<form method="post" action='register.php'>
-	Firstname: <input type="text" name="fname"><br>
-	Lastname: <input type="text" name="lname"><br>
-	Username: <input type="text" name="username"><br>
-	Addresse: <input type="text" name="address"><br>
-	City: <input type="text" name="city"><br>
-	Date of birth: <input type="text" name="dbo"><br>
-	Password: <input type="password" name="password"><br>
-	Re-type Password: <input type="password" name="repassword"><br>
-	<input type="submit" name="" value='Submit'>
-</form>
