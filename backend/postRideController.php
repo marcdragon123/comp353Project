3985d1c3b0198b719cef4ceb2665252c518252aa
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2016-11-30 09:42:40 -0500

    little changes on backend

--- backend/postRideController.php
+++ backend/postRideController.php
@@ -1,11 +1,12 @@
 <?php 
+	session_start();
 	include 'dbconnection.php';
   include 'dbFunctions.php';
-
+  	$licence = getUserLicense($_SESSION['user']);
 	print_r($_POST);
 
 	if ($_POST['tripType']=='singleTrip'){
-		echo postRideDB($_POST['distance']/10000,$_POST['dep_time'],$_POST['departure'],$_POST['destination'],'false',$_POST['dep_date'],"Trump for the win!",'1234',$_POST['distance']/1,$_POST['riders']);
+		echo postRideDB($_POST['distance']/10000,$_POST['dep_time'],$_POST['departure'],$_POST['destination'],'false',$_POST['dep_date'],"Trump for the win!",$licence,$_POST['distance']/1,$_POST['riders']);
 		header('Location:../core.php?message=success');
 	}
 	else{
@@ -14,7 +15,7 @@
 			$dow = $dow .$day . '-';
 		}
 		echo $dow;
-		echo postRideDB($_POST['distance']/10000,$_POST['dep_time'],$_POST['departure'],$_POST['destination'],'true','Fidel is dead!',$dow,"1234",$_POST['distance']/1,$_POST['riders']);
+		echo postRideDB($_POST['distance']/10000,$_POST['dep_time'],$_POST['departure'],$_POST['destination'],'true','Fidel is dead!',$dow,$licence,$_POST['distance']/1,$_POST['riders']);
 		header('Location:../core.php?message=success');
 
 	}
--- backend/takeRideController.php
+++ backend/takeRideController.php
@@ -4,6 +4,7 @@
 
 	print_r($_POST);
 	$msg =register_to_ride($_SESSION['user'],$_POST['r_id']);
+	echo $msg;
 	if($msg ==""){
 			header("Location:../core.php?message=m2");
 }
