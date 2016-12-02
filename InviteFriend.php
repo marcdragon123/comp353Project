e3064a8ee6cbcd8871f5d014fc9fd23d1f505247
Andres Kebe <i@andreskebe.com>
2016-12-01 23:41:28 -0500

    Added Back Button

--- InviteFriend.php
+++ InviteFriend.php
@@ -3,18 +3,28 @@
 
 <head>
     <meta charset="UTF-8">
+<!--Foundation-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.2.3/foundation.css">
+<!--Andres Stylesheet-->
     <link rel="stylesheet" href="stylesheet.css">
+<!--Tom Stylesheet    -->
     <link rel="stylesheet" href="./style/style.css">
+<!--Font-->
     <link href="https://fonts.googleapis.com/css?family=Fredoka+One|Poppins:500" rel="stylesheet">
     <title>Invite a Friend!</title>
 </head>
 
 <body>
     <?php include 'header.php';?>
-    <h3 id='friendH2'>Life is more fun when you bring your friends!<br/>You can invite your friends by using your code below!</h3>
+    <h3 id='friendH2'>Life is more fun when you bring your friends!
+        <br/>You can invite your friends by using your code below!
+    </h3>
     <div id='center'>
+<!--    Change 'Code' To the user code @Nunezz-->
         <div id='refCodeDiv'>CODE</div>
+    </div>
+    <div id='backButton'>
+        <a class="button" href="core.php">Go Back</a>
     </div>
 </body>
 </html>
